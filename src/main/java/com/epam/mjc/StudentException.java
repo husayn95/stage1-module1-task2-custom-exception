317e72572e9bf033daf627c260f5f18be121c2fe
Husayn Ganiev <hg222ii@student.lnu.se>
2023-02-21 13:09:54 +0100

    finished

---
+++ src/main/java/com/epam/mjc/StudentException.java
@@ -0,0 +1,10 @@
+package com.epam.mjc;
+
+public class StudentException extends IllegalArgumentException{
+
+    public StudentException(String message){
+        super(message);
+    }
+
+
+}
--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,17 +5,38 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
+
+  //public Student find(String studentID) throws StudentNotFoundException {
+  //    if (studentID.equals("123456")) {
+  //      return new Student();
+  //    } else {
+  //      throw new StudentNotFoundException(
+  //          "Could not find student with ID " + studentID);
+  //    }
+  //  }
+  public Student find(long studentID) throws StudentException{
+    StudentManager manager = new StudentManager();
+   // Student student = manager.find(studentID);
+    for (int i = 0; i < IDs.length; i++)
+      if (studentID == IDs[i])
+        studentID = IDs[i];
+      else throw new StudentException("Could not find student with ID" + studentID);
+
     return Student.getValueOf(studentID);
   }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
 
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
-    }
+    try {
 
+
+      for (int i = 0; i < IDs.length; i++) {
+        Student student = manager.find(IDs[i]);
+        System.out.println("Student name " + student.getName());
+      }
+    }  catch (StudentException ex) {
+    System.err.print(ex);
+  }
   }
 }
